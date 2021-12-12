d5852ec18317fb27fb03a021803899c934685f06
Angela Jackson <anjacks12@gmail.com>
2021-12-11 19:04:11 -0800

    added questions 6 and 7 to js

--- README.md
+++ README.md
@@ -18,11 +18,11 @@ This web page is an introduction to who I am. It includes a 5 question quiz that
 * I liked this lab because it was a nice way to ease into `if...else statements` and see how logical operators are used. I decided as a way to help me remember/learn, I tried to see if I could type out the html structure format myself but got stuck so I search for an example online. I did the same when writing the statements and also when incorporating the `.toLowerCase();` in the code but ended up having to refer to the class notes.
 * I think for me, some extra practice on assigning variables when writing statements and functions is going to be needed, especially since next week I get the feeling we will be diving into functions.
 * I wasn't sure how long this lab was going to take me, it ended up going quicker than I thought it would be, I'm hoping that will be par for the course but likely not, lol
-* How do you see yourself planning and evaluating your learning?
 
 ### Learning Journal 02
 
-* As in above, I'm going to assess each class if there are things I still don't understand in the discussion or find myself getting stuck on as a way to guide me to what I need to spend some extra time on learning
+* How do you see yourself planning and evaluating your learning?
+  * As in above, I'm going to assess each class if there are things I still don't understand in the discussion or find myself getting stuck on as a way to guide me to what I need to spend some extra time on learning
 * What details will you record and/or measure?
   * My plan is as I do or find ways to "test" myself, the amount of time it takes to solve a problem will be my gauge on how well I am learning or need to spend more time doing extra exercises
 * How often will you evaluate what you want to learn?
--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 //Asking user for name and giving a personalized greeting
 let username = prompt('What is your name?');
 //console.log(username);  //proof that code worked
-alert('Hello ' + username + '! Welcome to my page. Here are five questions to get to know me better.');
+alert('Hello ' + username + '! Welcome to my page. Here are seven questions to get to know me better.');
 
 
 //Question 1: Do I like reading? Assigning lowerLikeReading as variable to change likeReading input to lower case by using .toLowerCase()
@@ -26,28 +26,28 @@ if (lowerLikeReading === 'yes' || lowerLikeReading === 'y'){
 
 
 //Question 2: Is Korean my favorite food? Added .toLowerCase() after prompt to make code cleaner (via example given in class)
-let likeKoreanFood = prompt('Do I like Korean food? Yes or No').toLowerCase();
-//console.log(likeKoreanFood); //proof that code worked
-if (likeKoreanFood === 'yes' || likeKoreanFood === 'y'){
-  //console.log('Yes, I like Korean food');
-  alert('Yes, I like Korean food');
-} else if (likeKoreanFood === 'no' || likeKoreanFood === 'n'){
-  //console.log('No, I do like Korean food');
-  alert('No, I do like Korean food');
+let likeDriving = prompt('Do I like driving in Seattle? Yes or No').toLowerCase();
+//console.log(likeDriving); //proof that code worked
+if (likeDriving === 'yes' || likeDriving === 'y'){
+  //console.log('No. I actually hate driving downtown');
+  alert('No. I actually hate driving downtown. Parking is hard to find');
+} else if (likeDriving === 'no' || likeDriving === 'n'){
+  //console.log('Yes, I don't like driving in Seattle');
+  alert('Yes, I don\'t like driving in Seattle. I prefer the bus');
 } else {
   //console.log('Please answer yes or no');
   alert('Please answer yes or no');
 }
 
-//Question 3: Do I have siblings?
-let haveSibling = prompt('Do I have a sibling?').toLowerCase();
+//Question 3: Am I an only child?
+let haveSibling = prompt('Am I an only child?').toLowerCase();
 //console.log(haveSibling); //proof that code worked
 if (haveSibling === 'yes' || haveSibling === 'y'){
-  //console.log('Yes, I have a brother');
-  alert('Yes, I have a brother');
+  //console.log('No, I have a brother');
+  alert('No, I have a brother');
 } else if (haveSibling === 'no' || haveSibling === 'n'){
-  //console.log('No, I do have a sibling');
-  alert('No, I do have a sibling');
+  //console.log('Yes, I do have a sibling');
+  alert('Correct, I do have a brother');
 } else {
   //console.log('Please answer yes or no');
   alert('Please answer yes or no');
@@ -59,10 +59,10 @@ let nightOwl = prompt('Am I a night owl?').toLowerCase();
 //console.log(nightOwl);  //proof that code worked
 if (nightOwl === 'yes' || nightOwl === 'y'){
   //console.log('Yes, I\'m a night owl');
-  alert('Yes, I\'m a night owl');
+  alert('Yes, I\'m a night owl. Hoot, Hoot');
 } else if (nightOwl === 'no' || nightOwl === 'n'){
   //console.log('No, I am a night owl');
-  alert('No, I am a night owl');
+  alert('No, I like staying up late');
 } else {
   //console.log('Please answer yes or no');
   alert('Please answer yes or no');
@@ -77,12 +77,51 @@ if (likeCoffee === 'yes' || likeCoffee === 'y'){
   alert('Yes, I like coffee');
 } else if (likeCoffee === 'no' || likeCoffee === 'n'){
   //console.log('No, I do like coffee');
-  alert('No, I do like coffee');
+  alert('No, I LOVE coffee');
 } else {
   //console.log('Please answer yes or no');
   alert('Please answer yes or no');
 }
 
+//Question 6: user guesses a number; has 4 attempts to get it correct; prompt to display if guesses are too high or low; displays correct answer if guesses are incorrect; got help from Tyler, TA, on how placement of "--" to deincrament numOfAttempts
+let numOfAttempts = 4;
+while (numOfAttempts > 0){
+  let guess = prompt('How many do I have?');
+  let numberOfGuess = parseInt(guess);
+  //console.log(numberOfGuess); see user's input
+  //console.log(typeof(numberOfGuess)); to check that data type is number
+
+  if (numberOfGuess === 3){ //correct answer is 3
+    console.log('correct',numberOfGuess); //track user input
+    alert('You\'re right!');
+    break; // stops code once correct answer is entered
+  } else if (numberOfGuess > 3){
+    console.log('too high',numberOfGuess);  // track user input
+    numOfAttempts--; //reduces number of guess by 1
+    alert('Too high, try again');
+  } else if (numberOfGuess < 3){
+    console.log('Too low',numberOfGuess);  // track user input
+    alert('Too high, try again');
+    numOfAttempts--;
+  }
+}
+alert('The correct answer was 3'); //tells the user the correct answer is 3
+
+//Question 7: (got help from Ryan, instructor, on adding conditional expressions to for and while loops)
+let booksILike = ['thriller', 'mystery', 'historical fiction', 'crime', 'science fiction'];
+let attempts = 6;
+let correctAnswer = false; //set as false since answer likely to be false
+while (attempts > 0 && !correctAnswer){ //while loop runs when "attempts" is greater than 0 AND "correctAnswer" is true; that's the purpose of the "!" before correctAnswer so code can run
+let guesses = prompt('What are my favorite types of books?');
+attempts--;
+  for (let i = 0; i < 6; i++){
+    if (guesses === booksILike[i]){
+      alert('correct');
+      correctAnswer = true; //changed "correctAnswer" to true to exit out of loop when a correct answer is typed; this changes the value of !correctAnswer, which was true in line 114, to false so the code stops running
+    }
+  }
+}
+alert('I like reading ' + booksILike); //show the user all the possible answers
 
 //personalized parting message
 alert('Thanks again ' + username + ' for visiting my page. Please feel free to come back and check on any updates. Have a great day!');
